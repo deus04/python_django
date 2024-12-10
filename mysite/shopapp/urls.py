01034e0cd2520713a00c0b322291d50bd314c35f
Berdonosov Maksim <berdonosov_m@mail.ru>
2024-12-11 02:05:30 +0300

    Homework Mysite Django 2.6

---
+++ mysite/shopapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import shop_index
+
+
+app_name = 'shopapp'
+urlpatterns = [
+    path('', shop_index, name='index')
+]
