6c4a1afa5c583bb9474347ee424ef883830ce637
Berdonosov Maksim <berdonosov_m@mail.ru>
2025-01-15 05:15:24 +0300

    Homework Mysite Django 5.6

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shopapp.urls')),
+    path('req/', include('requestdataapp.urls')),
 ]
